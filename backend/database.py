c4315ce26c3c050388a69bedaf4ab1a14fe35020
Rajveer Singh Sisodia <rajsisodia446@gmail.com>
2026-04-11 12:23:53 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = "mysql+pymysql://root:rajveer@localhost:3306/demo"
+DB_URL = "mysql+pymysql://root:YOUR_PASSWORD@localhost:3306/demo"
 
 engine = create_engine(DB_URL, echo=True)
 
